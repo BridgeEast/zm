21840be593cb0d19986909042de9b737ed39085a
luxiaoyong <gdjyluxiaoyong@gmail.com>
2012-07-29 15:06:24 +0800

    the first commit of zm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zm::Application.config.secret_token = '73b51fbfc4bd4402105a73eb03a3d833bd230d0f54d2ccf02f0aee4f14f476baf6315604b45514feaa030a561f2e206e641b63b390111a4f821a5d9c85c0882c'
