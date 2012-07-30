e4cddcda9f9c7c269eee7d94445bf1ef0e92dc72
luxiaoyong <gdjyluxiaoyong@gmail.com>
2012-07-30 14:35:17 +0800

    修改登录界面，暂时屏蔽注册页面

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Zm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :to => "products#index"
   match ":controller/:action"
   match ":controller(/:action(/:id(.:format)))"
 
