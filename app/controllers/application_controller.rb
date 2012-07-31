2d3fc817154786d6f4b7eba0b029d1c187ea7034
luxiaoyong <gdjyluxiaoyong@gmail.com>
2012-07-31 20:00:50 +0800

    登录界面调用layout修改

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  layout :render_by_login_session
+  
+
+  def render_by_login_session
+    is_a?(Devise::SessionsController) ? "login" : "application"
+  end
+  
 end
