78bcc3a05ccbbd1bffbe72bcfd6eed0d45e21287
luxiaoyong <gdjyluxiaoyong@gmail.com>
2012-08-01 10:04:44 +0800

    代码修改

--- app/controllers/data_bases_controller.rb
+++ app/controllers/data_bases_controller.rb
@@ -14,25 +14,19 @@ class DataBasesController < ApplicationController
   #scope: data_bases/region/region.js
   def create_region
     Region.create!(params[:record])
-    respond_to do |format|
-      format.json{ render :json => {}}
-    end
+    render :json => {}
   end
 
   #scope: data_bases/region/region.js
   def delete_region
     Region.find(params[:id]).destroy
-    respond_to do |format|
-      format.json{ render :json => {}}
-    end
+      render :json => {}
   end
 
   #scope: data_bases/region/region.js
   def update_region
     Region.find(params[:record][:id]).update_attributes(params[:record])
-    respond_to do |format|
-      format.json{ render :json => {}}
-    end
+    render :json => {}
   end
 
 end
