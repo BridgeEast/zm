5a30014df0405cff90fcbee86bcac355ec1fa419
Francis <francis@ubuntu.ubuntu-domain>
2012-08-23 20:03:48 +0800

    修改

--- app/models/check_store_of_shoes.rb
+++
@@ -1,2 +0,0 @@
-class CheckStoreOfShoes < ActiveRecord::Base
-end
--- test/unit/check_store_of_shoes_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class CheckStoreOfShoesTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
